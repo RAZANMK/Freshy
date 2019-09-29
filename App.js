6192d5cd052edb3866facf3c3218d570dc7a0192
Abeer <a_mukhemir_1997@hotmail.com>
2019-09-29 13:43:38 +0300

    updated App.js

--- App.js
+++ App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>Step One, lets go!</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
